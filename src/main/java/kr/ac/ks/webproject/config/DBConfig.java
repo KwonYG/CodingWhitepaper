d4ca3146ee4a69a9eb319d10583930b9a6fda12f
KwonYG <andamirocll@gmail.com>
2018-10-02 20:06:04 +0900

    add properties

--- src/main/java/kr/ac/ks/webproject/config/DBConfig.java
+++ src/main/java/kr/ac/ks/webproject/config/DBConfig.java
@@ -3,8 +3,11 @@ package kr.ac.ks.webproject.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,25 +15,23 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 
 @Configuration
 @EnableTransactionManagement 
+@PropertySource(value = { "classpath:application.properties" })
 public class DBConfig implements TransactionManagementConfigurer {
-	private String driverClassName = "com.mysql.jdbc.Driver";
-
-	private String url = "jdbc:mysql://localhost:3306/projectdb?useUnicode=true&characterEncoding=utf8";
-
-	private String username = "dbuser";
-
-	private String password = "dbuser123!@#";
+	
+	@Autowired
+    private Environment environment;
 
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(driverClassName);
-		dataSource.setUrl(url);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
+
+		dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+
 		return dataSource;
 	}
-
 	@Override
 	public PlatformTransactionManager annotationDrivenTransactionManager() {
 		return transactionManger();
